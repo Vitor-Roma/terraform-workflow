165e85c758a13fcac9bd7d11ce2733bdcdc70ddb
Vitor <vitorvxroma@hotmail.com>
2024-07-24 18:24:33 -0300

    terraform backedn

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "terraform-bucket-test123"
+    key            = "test/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "my-lock-dynamodb-table"
   }
 }
