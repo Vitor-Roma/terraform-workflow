56f1087470ada98564bb379b1d034f477616ed21
Vitor <vitorvxroma@hotmail.com>
2024-07-24 17:35:06 -0300

    terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "meu-bucket-terraform"
+    key            = "caminho/para/meu/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "audit-log"
+  }
+}
