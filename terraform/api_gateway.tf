66f27d74729b71ffa87ea44d65b9fbb4c00e3802
Vitor <vitorvxroma@hotmail.com>
2024-08-29 17:25:47 -0300

    changes in cloudwatch

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -9,6 +9,24 @@ resource "aws_api_gateway_resource" "amm_api_resource" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_stage" "dsar_service" {
+  deployment_id        = aws_api_gateway_deployment.amm_api_deployment.id
+  rest_api_id          = aws_api_gateway_rest_api.amm_api_gateway.id
+  stage_name           = "prod"
+  xray_tracing_enabled = false # TODO: look into setting this to true
+  # TODO: look into canary settings
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.amm_service_access.arn
+    format          = "$context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] \"$context.httpMethod $context.resourcePath $context.protocol\" $context.status $context.responseLength $context.requestId $context.extendedRequestId"
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.amm_service_execution,
+    aws_cloudwatch_log_group.amm_service_access
+  ]
+}
+
 resource "aws_api_gateway_method" "amm_api_method" {
   rest_api_id   = aws_api_gateway_rest_api.amm_api_gateway.id
   resource_id   = aws_api_gateway_resource.amm_api_resource.id
--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -3,12 +3,12 @@ resource "aws_cloudwatch_log_group" "lambda" {
   retention_in_days = 90
 }
 
-resource "aws_cloudwatch_log_group" "dsar_service_execution" {
+resource "aws_cloudwatch_log_group" "amm_service_execution" {
   name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.amm_api_gateway.id}"
   retention_in_days = 90
 }
 
-resource "aws_cloudwatch_log_group" "dsar_service_access" {
+resource "aws_cloudwatch_log_group" "amm_service_access" {
   name              = "amazon-apigateway-access-logs_${aws_api_gateway_rest_api.amm_api_gateway.id}"
   retention_in_days = 90
 }
